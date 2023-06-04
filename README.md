620fd3fcea61f23ddb660e22c867ba6742b5f0a4
Fagner Lisboa <fnlisboa2016@gmail.com>
2023-06-04 19:02:49 -0300

    Pokedex-angular

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # AngularPokedex
 
+# Primeiro Pokedex
+
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.0.4.
 
 ## Development server
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PokemonCardComponent } from './pokemon-card/pokemon-card.component';
+import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonCardComponent,
+    PokemonListComponent
   ],
   imports: [
     BrowserModule,
